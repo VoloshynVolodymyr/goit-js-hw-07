1333514a47d42f0c798e0ce2e384c2c241df8bad
VoloshynVolodymyr <mailvoloshyn@gmail.com>
2022-11-02 15:15:46 +0200

    js-hw-07

---
+++ js/gallery-items.js
@@ -0,0 +1,170 @@
+export const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/06/18/18/26/holi-2416686_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/06/18/18/26/holi-2416686_1280.jpg',
+    description: 'India',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/08/12/16/59/parrot-3601194_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/08/12/16/59/parrot-3601194_1280.jpg',
+    description: 'Yellow ara',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/11/10/10/44/leaves-4615459_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/11/10/10/44/leaves-4615459_1280.jpg',
+    description: 'Autumn',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/07/29/18/38/parrot-5448628_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/07/29/18/38/parrot-5448628_1280.jpg',
+    description: 'Ara',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/08/29/18/50/coastal-5527726_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/08/29/18/50/coastal-5527726_1280.jpg',
+    description: 'Village',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/10/18/18/42/bulb-5665770_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/10/18/18/42/bulb-5665770_1280.jpg',
+    description: 'Flame and lamp',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/02/11/16/25/manarola-4840080_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/02/11/16/25/manarola-4840080_1280.jpg',
+    description: 'Manarola',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/10/01/13/53/droplet-3716288_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/10/01/13/53/droplet-3716288_1280.jpg',
+    description: 'Drop',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/11/09/14/01/sea-6781567_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/11/09/14/01/sea-6781567_1280.jpg',
+    description: 'Surge',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/07/04/20/12/green-lizard-5370821_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/07/04/20/12/green-lizard-5370821_1280.jpg',
+    description: 'Green lizard',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/05/18/15/44/apple-7205616_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/05/18/15/44/apple-7205616_1280.jpg',
+    description: 'Red apple',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/04/07/17/01/birds-5014150_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/04/07/17/01/birds-5014150_1280.jpg',
+    description: 'Chicks',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/04/18/14/01/blue-5059268_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/04/18/14/01/blue-5059268_1280.jpg',
+    description: 'Colorful fish',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/02/25/07/39/church-648430_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/02/25/07/39/church-648430_1280.jpg',
+    description: 'Winter in a city',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/04/11/03/33/dinant-2220459_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/04/11/03/33/dinant-2220459_1280.jpg',
+    description: 'Colorful city',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
